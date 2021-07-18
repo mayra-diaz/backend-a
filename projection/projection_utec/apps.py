5d0b2efe718c48705b7ddfaa89c2c538374d87ae
Alexandra-SR <alexandra.shulca@utec.edu.pe>
2021-07-18 16:43:30 -0500

    projection app

---
+++ projection/projection_utec/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProjectionUtecConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'projection_utec'
--- src/main/java/config/GlobalConstants.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-public class GlobalConstants {
-    /*User*/
-    public static final int DB_EMAIL_LENGTH = 500;
-    public static final int DB_AREA_LENGTH = 500;
-    public static final int DB_FIRST_NAME_LENGTH = 250;
-    public static final int DB_LAST_NAME_LENGTH = 250;
-}
